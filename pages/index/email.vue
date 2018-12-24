6b17c7e30e896132dd26f70f5d710bd72e058e6b
smallbee <smallbee3@gmail.com>
2018-12-24 09:57:36 +0900

    Nested Routes adjusted to index.vue

---
+++ pages/index/email.vue
@@ -0,0 +1,17 @@
+<template lang="html">
+  <div class="links">
+    <router-link to="/main"
+      class="button--green">cancel</router-link>
+    <a
+      href="https://github.com/nuxt/nuxt.js"
+      class="button--grey">Email</a>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,16 @@
+<template lang="html">
+  <div class="links">
+    <nuxt-link to="/facebook"
+      class="button--green">Facebook Login</nuxt-link>
+    <nuxt-link to="/email"
+      class="button--green">Login</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
