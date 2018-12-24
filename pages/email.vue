48aedca9f4b6899b92eae999dffaf7a2fe31639d
smallbee <smallbee3@gmail.com>
2018-12-25 08:43:28 +0900

    email.vue and sigup.vue added to pages folder

---
+++ pages/email.vue
@@ -0,0 +1,114 @@
+<template>
+  <section class="container">
+    <div>
+      <h1 class="title">
+        Email Login
+      </h1>
+
+      <form v-on:submit.prevent="loginSubmitBtn">
+        <input type="text" v-model="form.email" placeholder="Email">
+        <input type="password" v-model="form.password" placeholder="Password">
+        <button type="button" name="button">CANCEL</button>
+        <button type="submit" name="button">SUBMIT</button>
+        <div class="">
+          <span>{{ warningMessage }}</span>
+        </div>
+      </form>
+    </div>
+  </section>
+</template>
+
+<script>
+// const API_URL_ADDRESS = 'https://trello-api.smallbee.me';
+const API_URL_ADDRESS = 'http://localhost:8000';
+
+
+export default {
+  data() {
+    return {
+      form: {
+        email: '',
+        password: ''
+      },
+      warningMessage: ''
+    }
+  },
+  methods: {
+    setCookie(name,value,days) {
+        this.$cookies.set(name, (value || ""), {
+          path: '/',
+          maxAge: 60 * 60 * 24 * days
+        })
+    },
+    loginCancelBtn() {
+      console.log('backspace');
+    },
+    loginSubmitBtn(event) {
+      // console.log(this.setCookie);
+      // console.log(Cookie.methods.setCookie);
+      // Cookie.methods.setCookie();
+      const email = this.form.email.trim();
+      const password = this.form.password.trim();
+
+      // email validation
+      if (email === "" ||
+          email.indexOf('@') === -1) {
+        return this.warningMessage = 'Enter email correctly!';
+      }
+      // password validation
+      else if (password === "" ||
+               password.length < 8) {
+        return this.warningMessage = 'Enter password correctly!';
+      }
+      // http request
+      else {
+        console.log(email, password);
+        this.warningMessage = '';
+      }
+      console.log('Form submitted!')
+    }
+  }
+}
+</script>
+
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  display: block;
+  font-weight: 300;
+  font-size: 30px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+
+/* Custom */
+input {
+  font-size: 15px;
+}
+input:focus {
+  /* outline: none; */
+}
+
+
+
+</style>
